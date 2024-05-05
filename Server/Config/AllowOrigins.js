6f5c7c5140e945f366c10cb151e24b75146dacd0
alwayswantedtocode <101648314+alwayswantedtocode@users.noreply.github.com>
2024-05-05 22:32:56 +0100

    update

--- Server/Config/AllowOrigins.js
+++ Server/Config/AllowOrigins.js
@@ -3,6 +3,7 @@ const AllowOrigins = [
   "http://localhost:3001",
   "http://localhost:3002",
   "http://localhost:5010",
+  "https://lara-pastry-ui.onrender.com",
 ];
 
 module.exports = AllowOrigins;
